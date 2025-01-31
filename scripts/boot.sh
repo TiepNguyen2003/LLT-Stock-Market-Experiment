8b8af9ee85117678df91fc9db18002df82dbdd2a
Atlanmail <tiep123@trieuvan.com>
2025-01-31 15:32:49 -0800

    updated requirements and boot command

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # this script is used to boot a Docker container
 while true; do
-    #flask db init
-    #flask db migrate -m "First Migration"
+    flask db init
+    flask db migrate -m "First Migration"
     flask db upgrade
     if [[ "$?" == "0" ]]; then
         break
@@ -10,4 +10,3 @@ while true; do
     echo Deploy command failed, retrying in 5 secs...
     sleep 5
 done
-exec waitress-serve --host 0.0.0.0 --port=8000 --call app:create_app
