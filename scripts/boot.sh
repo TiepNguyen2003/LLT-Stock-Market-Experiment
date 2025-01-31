91a84f30eda86a10b0a5e8185e4655077bad7503
Atlanmail <tiep123@trieuvan.com>
2025-01-31 15:49:34 -0800

    Update boot.sh

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 # this script is used to boot a Docker container
-while true; do
-    flask db init
-    flask db migrate -m "First Migration"
-    flask db upgrade
-    if [[ "$?" == "0" ]]; then
-        break
-    fi
-    echo Deploy command failed, retrying in 5 secs...
-    sleep 5
-done
+flask db init
+flask db migrate -m "First Migration"
+flask db upgrade
